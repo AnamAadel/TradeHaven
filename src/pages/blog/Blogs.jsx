cdfea599a51fe293442fdf31cba7c855565838d1
Anam Aadel <rruholislam1998@gmail.com>
2023-12-14 22:20:34 +0600

    added home blog section dinamic

--- src/pages/blog/Blogs.jsx
+++ src/pages/blog/Blogs.jsx
@@ -27,9 +27,7 @@ function Blogs() {
 
                 ))}
                     
-                    <BlogCart data="24 Aug, 2021" heading="It is a long established factoi ader will be distracted" image='https://img.freepik.com/free-photo/cheerful-young-caucasian-businessman_171337-727.jpg?size=626&ext=jpg&ga=GA1.1.863241194.1678273173&semt=ais' />
-
-                    <BlogCart data="24 Aug, 2021" heading="Contrary to popular belieflo lorem Ipsum is not" image="https://img.freepik.com/free-photo/man-with-car-keys_1187-1058.jpg?w=826&t=st=1700404578~exp=1700405178~hmac=d84632a376e0e11a6731d8807fc4e47893e71b20c7ed752e3021aee0848053bf" />
+                    
                 </div>
                 <Pagination setPageNum={setPageNum} pageNum={pageNum} setData={setProductsData} showProductsNum={showProductsNum} />
             </div>
